3efa1fb13883885aae64ae829fbb941574cfcac1
ShorelineCrypto <admin@shorelinecrypto.com>
2025-01-11 13:49:42 -0500

    switch to eiquidus

--- run_nengexplorer.sh
+++ run_nengexplorer.sh
@@ -1,1 +1,1 @@
- docker run --rm -it --name nengexplorer -v ~/.nengcoin:/root/.nengcoin -p 3001:3001 nengexplorer:latest /bin/bash
+ docker run --net host --rm -it --name nengexplorer -v ~/.nengcoin:/root/.nengcoin -p 3001:3001 nengexplorer:latest /bin/bash
